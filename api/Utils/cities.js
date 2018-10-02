b891a1b59851c23e1b807fcb4914d09b93b84d0c
Francisco Borie <fjborie@miuandes.cl>
2018-10-02 19:47:34 -0300

    Cities geopoints saved on redis

---
+++ api/Utils/cities.js
@@ -0,0 +1,28 @@
+const cities = {
+    santiago: {
+        lat: -33.45,
+        lon: -70.6
+    },
+    zurich:{
+        lat: 47.3,
+        lon: 8.54
+    },
+    auckland:{
+        lat: -36.85,
+        lon: 174.78
+    },
+    sydney:{
+        lat: -33.87,
+        lon: 151.2
+    },
+    london:{
+        lat: 51.5,
+        lon: -0.1275
+    },
+    georgia:{
+        lat: 33.75,
+        lon: -84.39
+    }
+}
+
+export default cities;
--- api/app.js
+++ api/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import redis from 'redis';
+import cities from './Utils/cities';
 
 const app = express();
+
+const redisClient = redis.createClient();
+const citiesAsStrings = JSON.stringify(cities);
+redisClient.set("geopoints", citiesAsStrings, redis.print);
 
 export default app;
