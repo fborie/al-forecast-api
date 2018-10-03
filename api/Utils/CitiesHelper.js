cc4a1631d3e7af2eb2d7bf97ba48b5803e035801
Francisco Borie <fjborie@miuandes.cl>
2018-10-03 16:05:55 -0300

    Saved cities on CitiesHelper

---
+++ api/Utils/CitiesHelper.js
@@ -0,0 +1,38 @@
+const cities = [
+    {
+        key: "santiago",
+        lat: -33.45,
+        lon: -70.6
+    },
+    {
+        key: "zurich",
+        lat: 47.3,
+        lon: 8.54
+    },
+    {
+        key: "auckland",
+        lat: -36.85,
+        lon: 174.78
+    },
+    {
+        key: "sydney",
+        lat: -33.87,
+        lon: 151.2
+    },
+    {
+        key: "london",
+        lat: 51.5,
+        lon: -0.1275 
+    },
+    {
+        key: "atlanta",
+        lat: 33.75,
+        lon: -84.39
+    }
+];
+
+export const saveCities = (redisClient) => {
+    cities.forEach( city => {
+        redisClient.hset("cities", city.key, `${city.lat},${city.lon}`);
+    })
+}
--- api/app.js
+++ api/app.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import redis from 'redis';
-import cities from './Utils/cities';
 import ForecastApi from "./Utils/ForecastApi";
+import { saveCities } from './Utils/CitiesHelper';
 
 const apiUrl = 'https://api.darksky.net/forecast/40f80393884a99ecc7c1a2b0bc2a4952/';
 
 const app = express();
 
 const redisClient = redis.createClient();
-const citiesAsStrings = JSON.stringify(cities);
-redisClient.set("geopoints", citiesAsStrings, redis.print);
+
+saveCities(redisClient);
 
 const forecastApi = new ForecastApi(redisClient, apiUrl);
-forecastApi.getCityForecast(-33.45,-70.7).then(res => { console.log(res) });
+//forecastApi.getCityForecast(-33.45,-70.7).then(res => { console.log(res) });
 
 
 export default app;
